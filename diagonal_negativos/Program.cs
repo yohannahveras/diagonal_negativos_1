2f5edacdc0f23eb99b3896d6ec30e4044f85e877
Yohannah Véras <yohannahlv@gmail.com>
2023-06-10 16:43:18 -0300

    first commit

---
+++ diagonal_negativos/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int n, negativos;
+
+            Console.Write("Qual a ordem da matriz? ");
+            n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("Elemento [" + i + "," + j + "]: ");
+                    mat[i,j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("DIAGONAL PRINCIPAL:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+            Console.WriteLine();
+
+            negativos = 0;
+            for(int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i,j] < 0)
+                    {
+                        negativos++;
+                    }
+                }
+            }
+
+            Console.WriteLine("QUANTIDADE DE NEGATIVOS: " + negativos);
+
+        }
+    }
+}
